9a838d643436ae873e78bdc759363e7f08e04474
Joanna Hao <j.hao3@share.epsb.ca>
2023-01-03 13:22:52 -0700

    Forgot to commit and push progress; started writing new program, uploaded new flowchart.

--- main.py
+++ main.py
@@ -1,201 +1,254 @@
 """
-title: Spending/Expenditure Tracker
+title: Playlist Program
 author: Joanna Hao
-date-created: 2022-12-16
+date-created: 2023-01-01
 """
+from flask import Flask, render_template, request, redirect
+from pathlib import Path
 import sqlite3
-import pathlib
+
+"""
+reminders:
+- IB naming conventions: uppercase for all variables, camelCase for functions/methods
+- 
+"""
 
 
-# ----- SUBROUTINES ----- #
-# --- Inputs
-def getFileContent(filename):
-    
+# ---------- SUBROUTINES ---------- #
+# --- INPUTS --- #
+def getFileContent(FILENAME):
+    FILE = open(FILENAME)
+    CONTENT = FILE.readlines()
+    FILE.close()
+    # cleaning and reformatting data from file
+    for i in range(len(CONTENT)):
+        if CONTENT[i][-1] == "\n":
+            CONTENT[i] = CONTENT[i][:-1]
+        CONTENT[i] = CONTENT[i].split(",")
+        for j in range(len(CONTENT[i])):
+            if CONTENT[i][j].isnumeric():
+                CONTENT[i][j] = int(CONTENT[i][j])
+    return CONTENT
 
-def menu() -> int:
+
+def menu():
     print("""
-Please choose an option:
-1. View a table's data
-2. Modify some existing data
-3. Add new data set
-4. Calculate greatest expense & revenue source
+Choose an action:
+1. View all songs
+2. Add a song
+3. Modify an existing song's details
+4. Remove a song
+5. Search songs for potential duplicates
+6. Generate a playlist
+7. Have the computer guess your favourite artist or genre
     """)
-    choice = input("> ")
-    if choice.isnumeric() and 1 <= int(choice) <= 4:
-        return int(choice)
+    CHOICE = input("> ")
+    if CHOICE.isnumeric() and int(CHOICE) >= 1 and int(CHOICE) <= 7:
+        return int(CHOICE)
     else:
-        print("Please enter a valid number from the list")
+        print("Please enter a valid number from the list.")
         return menu()
 
-def getTableName():  # all options
-    print("""
-Please a select a table to interact with:
-1. Income
-2. Cost of Goods Sold
-3. Business Expenses
-4. Home Office & Home Operational Costs
-5. Automobile Expenses
-6. Bank Record
-7. Miscellaneous Information
-    """)
-    table = input("> ")
-    if table.isnumeric() and 1 <= int(table) <= 7:
-        return int(table)
-    else:
-        print("Please select a valid option from the list.")
-        return getTableName()
+
+def getNewSong():
+    while True:
+        SONG_NAME = input("Name of song? ")
+        if SONG_NAME == "":
+            print("Please enter a song name.")
+            continue
+        ARTIST = input("Name of artist? ")
+        if ARTIST == "":
+            print("Please enter an artist name.")
+            continue
+        COLLECTION = input("Is it part of an album or EP (1), or is it a single? (2) ")
+        if not (COLLECTION.isnumeric() and int(COLLECTION) >= 1 and int(COLLECTION) <= 2):
+            print("Please select a valid number.")
+            continue
+        else:
+            if COLLECTION == 1:
+                COLLECTION_NAME = input("What is the name of the album or EP? ")
+            else:
+                COLLECTION_NAME = "single"
+        DURATION = input("Duration of song? ")  # expecting X:XX or XX:XX form
+        if DURATION.find(":") == -1:
+            print("Please enter the duration of the song in the format 'X:XX' or 'XX:XX'.")
+            continue
+        GENRE = input("What is the genre of the song? Leave the field blank if you are unsure (and fill it in later).")
+        break
+
+    NEW_SONG = [SONG_NAME, ARTIST, COLLECTION_NAME, DURATION, GENRE]
+    for i in range(len(NEW_SONG)):
+        if NEW_SONG[i] == "":
+            NEW_SONG[i] = None
+    return NEW_SONG
 
 
-def getYear():  # option 3 (?), 4
-    pass
 
-
-
-
-# --- Processing
-def setupContent():
+# --- PROCESSING --- #
+def setupSongs(DATA_LIST):
     global CURSOR, CONNECTION
     CURSOR.execute("""
         CREATE TABLE
-            income (
-                date INTEGER PRIMARY KEY,
-                sales FLOAT,
-                commissions_fees FLOAT,
-                professional_income FLOAT,
-                sales_tax FLOAT
+            favourite_songs (
+                id INTEGER PRIMARY KEY,
+                song_name TEXT NOT NULL,
+                artist TEXT NOT NULL,
+                collection_name TEXT,
+                duration TEXT,
+                genre TEXT
             )
     ;""")
-
-    CURSOR.execute("""
-        CREATE TABLE
-            cost_goods_sold (
-                date INTEGER PRIMARY KEY,
-                opening_inventory FLOAT,
-                year_purchases FLOAT,
-                direct_wage_costs FLOAT,
-                subcontracts FLOAT,
-                other_costs FLOAT,
-                closing_inventory FLOAT,
-                sales_tax FLOAT
+    for i in range(1, len(DATA_LIST)):
+        CURSOR.execute("""
+            INSERT INTO
+                favourite_songs
+            VALUES (
+                ?, ?, ?, ?, ?, ?
             )
-    ;""")
+        ;""", DATA_LIST[i])
+    CONNECTION.commit()
 
+
+def getAllSongsBasic():
+    global CURSOR
+    SONGS = CURSOR.execute("""
+        SELECT
+            song_name,
+            artist
+        FROM
+            favourite_songs
+    ;""").fetchall()  # returns a list of tuples
+
+    for i in range(len(SONGS)):
+        print(f"{i + 1}. {SONGS[0]} by {SONGS[1]}")  # of the format: 1. (song_name) by (artist)
+
+
+def insertNewSong(SONG_DETAILS):
+    ### worried about inserting list-in-a-list (which would only fill the 1st spot) if [SONG_DETAILS]
+    global CURSOR, CONNECTION
     CURSOR.execute("""
-        CREATE TABLE
-            expenses (
-                date INTEGER PRIMARY KEY,
-                advertising FLOAT,
-                meals_entertainment FLOAT,
-                bad_debts FLOAT,
-                insurance FLOAT,
-                interest FLOAT,
-                provincial_license_fee FLOAT,
-                memberships_fees FLOAT,
-                business_taxes FLOAT,
-                govt_fees FLOAT,
-                office_expenses FLOAT,
-                supplies FLOAT,
-                professional_legal_fees FLOAT,
-                management_admin fees FLOAT,
-                rent FLOAT,
-                maintenance_repairs FLOAT,
-                sub_contractors FLOAT,
-                computer_expenses FLOAT,
-                salary FLOAT,
-                travel FLOAT,
-                phone_utilities FLOAT,
-                internet_cable FLOAT,
-                delivery_freight FLOAT,
-                capital_cost_allowance FLOAT,
-                other FLOAT
-            )
-    ;""")
-
-    CURSOR.execute("""
-        CREATE TABLE
-            home (
-                date INTEGER PRIMARY KEY,
-                home_sq_footage FLOAT,
-                office_sq_footage FLOAT,
-                office_percent_of_home FLOAT GENERATED ALWAYS AS (
-                    office_aq_footage / home_sq_footage * 100
-                ),
-                heat FLOAT,
-                electricity_water FLOAT,
-                insurance FLOAT,
-                maintenance FLOAT,
-                mortgage_interest FLOAT,
-                property taxes FLOAT,
-                phone FLOAT,
-                internet FLOAT,
-                cable FLOAT,
-                other_expenses FLOAT,
-                personal_use_area FLOAT
-            )
-    ;""")
-
-    CURSOR.execute("""
-        CREATE TABLE
-            automobile_expenses (
-                date INTEGER PRIMARY KEY,
-                driven_distance_income INTEGER,
-                total_distance_driven INTEGER,
-                fuel FLOAT,
-                vehicle_maintenance FLOAT,
-                lease_payments FLOAT,
-                washes FLOAT,
-                insurance FLOAT,
-                license_registration FLOAT,
-                vehicle_purchase_interest FLOAT,
-                etr_407 FLOAT,
-                caa FLOAT,
-                capital_allowance FLOAT
-            )
-    ;""")  # annual costs (not monthly)
-
-    CURSOR.execute("""
-        CREATE TABLE
-            bank_record (
-                date INTEGER PRIMARY KEY,
-                description TEXT,
-                deposit FLOAT,
-                charge FLOAT,
-                balance FLOAT
-            )
-    ;""")
+        INSERT INTO
+            favourite_songs
+        VALUES (
+            ?, ?, ?, ?, ?, ?
+        )
+    ;""", SONG_DETAILS)
+    CONNECTION.commit()
 
 
-def getOneTableData():  # option 1, 2/3/4
+def getAllSongsComplete():
+    # used for: modifying song details
+    global CURSOR
+    SONGS = CURSOR.execute("""
+            SELECT
+                song_name,
+                artist,
+                collection_name,
+                duration,
+                genre
+            FROM
+                favourite_songs
+        ;""").fetchall()  # returns a list of tuples
+
+    for i in range(len(SONGS)):
+        # of the format: 1. song_name by artist on collection_name (duration)
+        print(f"{i + 1}. {SONGS[0]} by {SONGS[1]} ", end="")
+        if SONGS[2] == "single":
+            print(f"({SONGS[3]})")  # just duration
+        else:
+            print(f"on {SONGS[2]} ({SONGS[3]})")  # print album as well
+        if SONGS[-1] != "":
+            print(f"Genre: {SONGS[-1]}")
+        else:
+            print(f"Genre: ?")
+
+
+def getSongID():
+    # used for modifying song details:
     pass
 
 
-def getAllTableData():  # option 1
-    pass
+# --- OUTPUTS --- #
 
 
-def getTableColNames():  # option 2, 3, 4
-    pass
-
-
-# --- Outputs
-
-
-
-# --- variables
-DATABASE_FILE = "business_finance_tracker.db"
+# ----- VARIABLES ----- #
+DB_NAME = "favourite_songs.db"
 FIRST_RUN = True
-if (pathlib.Path.cwd() / DATABASE_FILE).exists():
+if (Path.cwd() / DB_NAME).exists():
     FIRST_RUN = False
 
-CONNECTION = sqlite3.connect(DATABASE_FILE)
+CONNECTION = sqlite3.connect(DB_NAME)
 CURSOR = CONNECTION.cursor()
 
-INITIAL_DATA_FILES = ["file1.csv", "file2.csv", "file3.csv", ]
+"""
+# --- FLASK --- #
+app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# app.run(debug=True)  # in main program code section 
+"""
 
 if __name__ == "__main__":
     # ----- MAIN PROGRAM CODE ----- #
     if FIRST_RUN:
-        # setup everything
-        CONTENT = getFileContent("INITIAL_DATA.csv")  # modify name later
-        setupContent(CONTENT)
+        CONTENT = getFileContent("filename.ext")  ### MODIFY LATER
+        setupSongs(CONTENT)
+
+    while True:
+        print("Welcome to your playlist program!")
+        # --- inputs
+        CHOICE = menu()
+        if CHOICE == 2:  # add song
+            pass
+        elif CHOICE == 3:  # modify song
+            pass
+        elif CHOICE == 4:  # remove song
+            pass
+        elif CHOICE == 5:  # duplicates
+            pass  # will have 2 inputs to collect
+        elif CHOICE == 6:  # generate playlist
+            pass
+        else:  # guess favourites
+            pass
+
+        # --- processing
+        if CHOICE == 1:  # see all songs
+            pass
+        elif CHOICE == 2:  # add song
+            pass
+        elif CHOICE == 3:  # modify song
+            pass
+        elif CHOICE == 4:  # remove song
+            pass
+        elif CHOICE == 5:  # duplicates
+            pass
+        elif CHOICE == 6:  # generate playlist
+            pass
+        else:  # guess favourite ____
+            pass
+
+        # --- outputs
+        if CHOICE == 1:  # see all songs
+            pass
+        elif CHOICE == 2:  # add song
+            pass
+        elif CHOICE == 3:  # modify song
+            pass
+        elif CHOICE == 4:  # remove song
+            pass
+        elif CHOICE == 5:  # duplicates
+            pass
+        elif CHOICE == 6:  # generate playlist
+            pass
+        else:  # guess favourite ____
+            pass
+
+"""
+Current progress: (2023/01/03)
+- finished CHOICE 1, 2
+- working on CHOICE 3 
+
+"""
